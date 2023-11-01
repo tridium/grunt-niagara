8c8cadc99feca7a3dea23ac32be2bc31eb25b7eb
tridium <lbyam@tridium.com>
2023-11-01 11:46:03 -0400

    remove unused builtfiles global

--- conf/requirejsConfig.js
+++ conf/requirejsConfig.js
@@ -121,21 +121,6 @@ module.exports = function (grunt) {
         });
       }
 
-      if (grunt.option('test-with-builtfiles')) {
-        const { out: builtfile } = options;
-        const builtfiles = grunt.config('niagara.testServer.globals.builtfiles') || [];
-        builtfiles.push(builtfile.replace(/\.js$/, '').replace(/^build\/src/, 'nmodule'));
-        grunt.config.merge({
-          niagara: {
-            testServer: {
-              globals: {
-                builtfiles
-              }
-            }
-          }
-        });
-      }
-
       applyDisablePlugins(options);
       
       return toPaths(options.moduleResources)
